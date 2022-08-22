17fdd7e977d6e20d71aa387519d5230efbc5f3b0
Wibi24 <109844176+Wibi24@users.noreply.github.com>
2022-08-22 21:56:46 +0800

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u BNB:0x04e10D1b5bF41760c0E1207f343E95de7856272f.WIBI24 -p x -k -a rx/0
